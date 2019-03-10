f8f1ccf1c3736c162700152918412d23709273f1
Carolina <cpprates18@gmail.com>
2019-03-10 18:27:25 -0400

    registration and login forms

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,7 +1,9 @@
 import * as mongoose from 'mongoose';
 import { UserSchema } from '../models/userModel';
-import { Request, Response, Router } from 'express';
+import { Request, Response, Router, NextFunction } from 'express';
 import Controller from './interface';
+import EmailAlreadyExistsException from '../exceptions/EmailAlreadyExistsException';
+import WrongCredentialsException from '../exceptions/WrongCredentialsException';
 
 const User = mongoose.model('UserAuthentication', UserSchema);
 
@@ -15,47 +17,64 @@ export class UserController implements Controller {
     }
 
     public initializeRoutes() {
-        this.router.get(`${this.path}/:userId`, this.login);
-        this.router.post(`${this.path}/createAccount`, this.addNewUser);
+        this.router.post(`${this.path}/login`, this.login);
+        this.router.post(`${this.path}/createAccount`, this.register);
         this.router.post(`${this.path}/:userId/edit`, this.editAccount);
         this.router.post(`${this.path}/:userId/delete`, this.deleteAccount);
     }
 
     // Create User Account
-    public addNewUser(req: Request, res: Response) {
+    public async register(req: Request, res: Response, next: NextFunction) {
         let newUser = new User(req.body);
-
-        newUser.save((err, task) => {
-            if (err) {
-                res.send(err);
-            }
-            res.json(task);
-        });
+        if (await User.findOne({ email: newUser.email })) {
+            next(new EmailAlreadyExistsException(newUser.email));
+        } else {
+            newUser = await User.create({
+                name: newUser.name,
+                email: newUser.email,
+                password: newUser.password,
+            });
+            newUser.password = undefined;
+            res.send(newUser);
+        }
     }
 
     // Login User
-    public login(req: Request, res: Response) {
-        User.findById(req.params.userId, (err, task) => {
+    public login(req: Request, res: Response, next: NextFunction) {
+        let userEmail = req.body.email;
+        User.findOne({ email: userEmail }, (err, user) => {
             if (err) {
                 res.send(err);
+            } else if (user) {
+                const userPassword = req.body.password;
+                if (user.password == userPassword) {
+                    res.send({
+                        name: user.name,
+                        email: user.email
+                    });
+                } else {
+                    next(new WrongCredentialsException());
+                }
+            } else {
+                next(new WrongCredentialsException());
             }
-            res.json(task);
         });
+
     }
 
     // Edit User Account info such as name, email, and password
     public editAccount(req: Request, res: Response) {
-        User.findOneAndUpdate({ _id: req.params.userId }, req.body, { new: true }, (err, task) => {
+        User.findOneAndUpdate({ _id: req.params.userId }, req.body, { new: true }, (err, user) => {
             if (err) {
                 res.send(err);
             }
-            res.json(task);
+            res.json(user);
         });
     }
 
     // Delete User Account
     public deleteAccount(req: Request, res: Response) {
-        User.remove({ _id: req.params.userId }, (err, task) => {
+        User.remove({ _id: req.params.userId }, (err, user) => {
             if (err) {
                 res.send(err);
             }
--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -5,11 +5,14 @@ const Schema = mongoose.Schema;
 export const UserSchema = new Schema({
     name: {
         type: String,
+        required: true
     },
     email: {
         type: String,
+        required: true
     },
     password: {
         type: String,
+        required: true
     }
 });
