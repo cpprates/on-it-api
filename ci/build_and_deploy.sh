7d3b567d0f30e54187789c0227d2c904973c1210
Juliano <julianosam@gmail.com>
2019-03-18 02:52:00 -0400

    Uncommenting script

--- ci/build_and_deploy.sh
+++ ci/build_and_deploy.sh
@@ -11,14 +11,14 @@ ENV_FILE=./ci/env
 chmod 400 ${PEM_FILE}
 ssh-add ${PEM_FILE}
 
-# rm ${IMAGE_NAME}.tar.gz || true
+rm ${IMAGE_NAME}.tar.gz || true
 
 echo "Building and deploying $IMAGE_NAME..."
-# docker build -t ${IMAGE_NAME} .
-# docker save ${IMAGE_NAME} | gzip > ${IMAGE_NAME}.tar.gz
+docker build -t ${IMAGE_NAME} .
+docker save ${IMAGE_NAME} | gzip > ${IMAGE_NAME}.tar.gz
 
 ssh ${USER}@${HOST} 'mkdir -p /opt/onit/workdir'
-# scp ${IMAGE_NAME}.tar.gz ${USER}@${HOST}:/opt/onit/workdir
+scp ${IMAGE_NAME}.tar.gz ${USER}@${HOST}:/opt/onit/workdir
 scp ${ENV_FILE} ${USER}@${HOST}:/opt/onit/workdir
 ssh ${USER}@${HOST} 'bash -s' < ./ci/remote_restart.sh
 
