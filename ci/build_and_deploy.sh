e1aa54803b2f49c20d9ac2c4ad0f4d42fc2641bc
Juliano <julianosam@gmail.com>
2019-03-18 02:48:28 -0400

    Moving environment variables out of the application.

--- ci/build_and_deploy.sh
+++ ci/build_and_deploy.sh
@@ -5,19 +5,21 @@ USER=ec2-user
 HOST=ec2-18-204-24-255.compute-1.amazonaws.com
 IMAGE_NAME=onit-api-image
 PEM_FILE=./ci/global_key.pem
+ENV_FILE=./ci/env
 
 # Install cert
 chmod 400 ${PEM_FILE}
 ssh-add ${PEM_FILE}
 
-rm ${IMAGE_NAME}.tar.gz || true
+# rm ${IMAGE_NAME}.tar.gz || true
 
 echo "Building and deploying $IMAGE_NAME..."
-docker build -t ${IMAGE_NAME} .
-docker save ${IMAGE_NAME} | gzip > ${IMAGE_NAME}.tar.gz
+# docker build -t ${IMAGE_NAME} .
+# docker save ${IMAGE_NAME} | gzip > ${IMAGE_NAME}.tar.gz
 
 ssh ${USER}@${HOST} 'mkdir -p /opt/onit/workdir'
-scp ${IMAGE_NAME}.tar.gz ${USER}@${HOST}:/opt/onit/workdir
+# scp ${IMAGE_NAME}.tar.gz ${USER}@${HOST}:/opt/onit/workdir
+scp ${ENV_FILE} ${USER}@${HOST}:/opt/onit/workdir
 ssh ${USER}@${HOST} 'bash -s' < ./ci/remote_restart.sh
 
 
--- ci/remote_restart.sh
+++ ci/remote_restart.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+ENV_FILE=/opt/onit/workdir/env
 IMAGE=onit-api-image
 
 echo "Stopping $IMAGE if running..."
@@ -11,5 +11,7 @@ docker rm $IMAGE || true
 
 # run your container
 echo "Loading the new image $IMAGE..."
+source $ENV_FILE
 docker load < /opt/onit/workdir/$IMAGE.tar.gz
-docker run --name $IMAGE -p 80:3000 --restart always -d $IMAGE
+docker run --name $IMAGE -p 80:3000 --restart always -e DB_URL=$DB_URL -e SECRET=$SECRET -d $IMAGE
+rm $ENV_FILE
